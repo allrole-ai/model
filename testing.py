3bc3c32c348f578979e5e92ecd81dd2adf46f9e4
fahad17 <fahadra96@gmail.com>
2024-07-07 01:13:25 +0700

    testing dataset

---
+++ testing.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from transformers import TFBertForSequenceClassification, BertTokenizer
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+import csv
+
+# Load the tokenizer and model
+tokenizer = BertTokenizer.from_pretrained('indobert_model')
+model = TFBertForSequenceClassification.from_pretrained('indobert_model')
+
+# Load the label encoder
+with open('qa.csv', 'r', encoding='utf-8') as file:
+    reader = csv.reader(file, delimiter='|')
+    filtered_rows = [row for row in reader if len(row) == 2]
+
+df = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
+label_encoder = LabelEncoder()
+label_encoder.fit(df['answer'])
