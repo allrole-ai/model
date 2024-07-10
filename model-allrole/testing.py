493e02c0aa22fae710d56ab16a84b9585b12c463
fahad17 <fahadra96@gmail.com>
2024-07-10 22:12:07 +0700

    vectorizer data

--- model-allrole/testing.py
+++ model-allrole/testing.py
@@ -1,3 +1,10 @@
 import pickle
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+
+# Memuat vectorizer dan data yang telah disimpan
+with open('model/vectorizer.pkl', 'rb') as f:
+    vectorizer = pickle.load(f)
+
+with open('model/data.pkl', 'rb') as f:
+    data = pickle.load(f)
