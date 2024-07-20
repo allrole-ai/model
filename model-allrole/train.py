48c6a317eb7f381175e4d23ab3197663dff3578a
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-20 19:54:21 +0700

    contoh menggunakan prediksi

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -88,3 +88,12 @@ def predict(question, answer):
 
     return predicted_class
 
+#Example using predictions
+question = "cara minta transkrip nilai"
+answer = "Yth. Kepala Bagian Akademik Universitas XYZdi TempatDengan hormat,Mahasiswa Universitas XYZ, Nama saya [Nama], dengan NIM [NIM]. Saya ingin meminta transkrip nilai semester [semester] yang telah saya tempuh.Demikian surat permohonan ini saya sampaikan, atas perhatian dan kerjasamanya saya ucapkan terima kasih.Hormat saya [Nama]"
+
+predicted_class = predict(question, answer)
+print(f"Pertanyaan: {question}")
+print(f"Jawaban: {answer}")
+print(f"Kelas Prediksi: {predicted_class}")
+
