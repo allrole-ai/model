e5dd74e8b94cc5291bfe3659c2d75277fc2140ed
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-20 19:45:16 +0700

    add training and testing model

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -7,3 +7,10 @@ import torch
 
 # load dataset train
 df = pd.read_csv('lar-clean.csv', delimiter='|', names=['question', 'answer'], encoding='utf-8', quoting=csv.QUOTE_NONE)
+
+# Buat label biner (0 atau 1) dari data jawaban jika perlu
+df['label'] = df.index % 2  # For example, using indexes as temporary labels
+
+# Pisahkan data menjadi pelatihan dan pengujian
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+
