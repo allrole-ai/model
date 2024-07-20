e6d43fb3a98490b38273ef4ef63d4cbcb1ff4a66
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-20 19:43:15 +0700

    add import

---
+++ model-allrole/train.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import csv
+from transformers import RobertaTokenizer, RobertaForSequenceClassification, Trainer, TrainingArguments, DataCollatorWithPadding
+from sklearn.model_selection import train_test_split
+from datasets import Dataset
+import torch
