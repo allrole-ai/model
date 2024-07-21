3d1d67a375c5a701fc04581c4624db7c02edd394
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-21 18:56:21 +0700

    add encoder

--- model-allrole/testing.py
+++ model-allrole/testing.py
@@ -8,3 +8,12 @@ import csv
 tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
 model = TFAlbertForSequenceClassification.from_pretrained('albert-base-v2')
 
+# Load Encoder label
+with open('rf1.csv', 'r', encoding='utf-8') as file:
+    reader = csv.reader(file, delimiter='|')
+    filtered_rows = [row for row in reader if len(row) == 2 and row[0].strip() != "" and row[1].strip() != ""]
+
+df = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
+label_encoder = LabelEncoder()
+label_encoder.fit(df['answer'])
+
