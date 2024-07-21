d07e0018302a9e45f2458e1f58ba4bcf41262781
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-21 18:58:06 +0700

    add test model

--- model-allrole/testing.py
+++ model-allrole/testing.py
@@ -48,3 +48,29 @@ with open('rf1.csv', 'r', encoding='utf-8') as file:
 
 df_test = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
 df_test['encoded_answer'] = label_encoder.transform(df_test['answer'])
+
+input_ids_test = []
+attention_masks_test = []
+for question in df_test['question']:
+    encoded = tokenizer.encode_plus(
+        question,
+        add_special_tokens=True,
+        max_length=128,
+        padding='max_length',
+        truncation=True,
+        return_attention_mask=True,
+        return_tensors='tf'
+    )
+    input_ids_test.append(encoded['input_ids'])
+    attention_masks_test.append(encoded['attention_mask'])
+
+input_ids_test = tf.concat(input_ids_test, axis=0)
+attention_masks_test = tf.concat(attention_masks_test, axis=0)
+labels_test = tf.constant(df_test['encoded_answer'].values)
+
+logits_test = model(input_ids_test, attention_mask=attention_masks_test).logits
+preds_test = tf.argmax(logits_test, axis=1, output_type=tf.int32)
+test_accuracy_metric.update_state(labels_test, preds_test)
+
+test_accuracy = test_accuracy_metric.result()
+print(f"Test accuracy: {test_accuracy:.4f}")
