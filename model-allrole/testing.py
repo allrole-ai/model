41b1e546c190b2f0203a903baf9b2e5fd46e52f8
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-21 18:39:06 +0700

    remove vectorizer

--- model-allrole/testing.py
+++ model-allrole/testing.py
@@ -1,20 +1,5 @@
 
 
-# Memuat vectorizer dan data yang telah disimpan
-with open('model/vectorizer.pkl', 'rb') as f:
-    vectorizer = pickle.load(f)
-
-with open('model/data.pkl', 'rb') as f:
-    data = pickle.load(f)
-
-
-def get_response(query):
-    query_vec = vectorizer.transform([query]).toarray()
-    data_vecs = vectorizer.transform(data['question']).toarray()
-    similarities = cosine_similarity(query_vec, data_vecs).flatten()
-    closest_idx = np.argmax(similarities)
-    return data['answer'].iloc[closest_idx]
-
 # Loop untuk menerima pertanyaan terus-menerus
 while True:
     pertanyaan = input("Masukkan pertanyaan (ketik 'exit' untuk keluar): ")
