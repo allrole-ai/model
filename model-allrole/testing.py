17ca23f327b85ba5f3c08e1112fb51569b8ec76d
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-21 18:57:43 +0700

    add test model

--- model-allrole/testing.py
+++ model-allrole/testing.py
@@ -41,3 +41,10 @@ def predict(question):
 # Hitung akurasi Pada Set Tes
 test_accuracy_metric = tf.keras.metrics.Accuracy()
 
+# test model
+with open('rf1.csv', 'r', encoding='utf-8') as file:
+    reader = csv.reader(file, delimiter='|')
+    filtered_rows = [row for row in reader if len(row) == 2 and row[0].strip() != "" and row[1].strip() != ""]
+
+df_test = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
+df_test['encoded_answer'] = label_encoder.transform(df_test['answer'])
