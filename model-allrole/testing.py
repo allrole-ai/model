ab9f796fcdcc3fbb9f9e53ca260b761ca253c91b
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-21 18:55:00 +0700

    add load tokenizer

--- model-allrole/testing.py
+++ model-allrole/testing.py
@@ -1,7 +1,10 @@
 import tensorflow as tf
-from transformers import TFRobertaForSequenceClassification, RobertaTokenizer
+from transformers import TFAlbertForSequenceClassification, AlbertTokenizer
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import csv
 
+# Load tokenizer dan model
+tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
+model = TFAlbertForSequenceClassification.from_pretrained('albert-base-v2')
 
