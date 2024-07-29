a6e385c0ccf77fa790b59a2fd2eabd336c1cf8b3
fahad17 <fahadra96@gmail.com>
2024-07-29 23:18:37 +0700

    refactor func main

--- model-allrole/testing.py
+++ model-allrole/testing.py
@@ -15,12 +15,11 @@ def main():
     tokenizer, model = load_model_and_tokenizer()
     
     while True:
-    question = input("Masukkan pertanyaan (atau ketik 'exit' untuk keluar): ")
+        question = input("Masukkan pertanyaan (atau ketik 'exit' untuk keluar): ")
         if question.lower() == 'exit':
             break
-    response = generate_response(tokenizer, model, question)
+        response = generate_response(tokenizer, model, question)
         print(f"Answer: {response}\n")
-
 
 if __name__ == "__main__":
     main()
