584c5538dc6080510a388531c0df5c455a6fef34
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 00:20:13 +0700

    delete function to answer new questions

--- model-allrole/training.py
+++ model-allrole/training.py
@@ -44,6 +44,3 @@ def answer_question(question, context=[]):
     """
     context.append(question)
     input_text = " ".join(context)
-    result = nlp(input_text)
-    label = max(result[0], key=lambda x: x['score'])['label']
-    return id2label[int(label.split('_')[-1])]
