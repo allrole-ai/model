4fa8ed1039e3bbb137aa0442bc8c12eb01f68ccb
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 00:49:57 +0700

    delete Trainer with model, arguments, data collator, train dataset, and eval dataset

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -26,13 +26,7 @@
 
 
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    data_collator=data_collator,
-    train_dataset=train_dataset,
-    eval_dataset=eval_dataset,
-)
+
 
 trainer.train()
 
