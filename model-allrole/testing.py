838c362d4ca1b5dab35de2d5fbbc542ac9623e41
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 00:32:58 +0700

    delete libraries

--- model-allrole/testing.py
+++ model-allrole/testing.py
@@ -1,5 +1,4 @@
-import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
+
 
 def load_model_and_tokenizer(model_path='./trained_model'):
     tokenizer = AutoTokenizer.from_pretrained(model_path)
