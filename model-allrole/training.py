f68d29dd4be94cdcc9d0c996acba08f6ce6b2fa0
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 00:31:55 +0700

    Define function to answer new questions using trained model and tokenizer

--- model-allrole/training.py
+++ model-allrole/training.py
@@ -65,3 +65,22 @@ if not os.path.exists('model'):
     os.makedirs('model')
 model.save_pretrained('model/qa_model')
 tokenizer.save_pretrained('model/qa_tokenizer')
+
+nlp = pipeline('text-classification', model=model, tokenizer=tokenizer, return_all_scores=True)
+
+def answer_question(question, context=[]):
+    """
+    Menjawab pertanyaan berdasarkan konteks percakapan sebelumnya.
+    
+    Parameters:
+    - question (str): Pertanyaan yang diajukan.
+    - context (list): Daftar konteks percakapan sebelumnya.
+    
+    Returns:
+    - str: Jawaban yang dihasilkan oleh model.
+    """
+    context.append(question)
+    input_text = " ".join(context)
+    result = nlp(input_text)
+    label = max(result[0], key=lambda x: x['score'])['label']
+    return id2label[int(label.split('_')[-1])]
