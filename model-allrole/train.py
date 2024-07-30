4da1943013915e653595689cf33315ffca382060
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 00:51:59 +0700

    delete the model with example questions and print the generated responses

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -35,11 +35,3 @@
 
 
 
-test_questions = [
-    "cara bayar spp",
-    "cara buka Menu"
-]
-
-for question in test_questions:
-    print(f"Question: {question}")
-    print(f"Answer: {generate_response(question)}\n")
