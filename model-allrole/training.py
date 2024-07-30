02ec34096fb7f6a31de53dacd77096049c577f08
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 00:16:40 +0700

    delete trainer arguments

--- model-allrole/training.py
+++ model-allrole/training.py
@@ -21,9 +21,7 @@ training_args = TrainingArguments(
     output_dir='./results',
     num_train_epochs=3,
     per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    warmup_steps=500,
-    weight_decay=0.01,
+
 
 
 trainer = Trainer(
