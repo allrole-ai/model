d02b3946298d69e0d40fe05873a69def23a0195e
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 00:55:37 +0700

    Load DistilBERT tokenizer and model

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -10,3 +10,6 @@ qa_data = pd.read_csv('dataset/qa-dataset.csv', delimiter='|')
 qa_data = qa_data.dropna().reset_index(drop=True)
 
 train_texts, val_texts, train_labels, val_labels = train_test_split(qa_data['question'], qa_data['answer'], test_size=0.2, random_state=42)
+
+tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', num_labels=len(qa_data['answer'].unique()))
