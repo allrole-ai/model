771b74618ce2e20ea5ef1cea1e7e3388bec2a48c
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 00:51:34 +0700

    delete

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -32,11 +32,7 @@
 
 
 
-# Langkah 4: Pengujian dan Evaluasi Model
-def generate_response(question):
-    inputs = tokenizer.encode(question, return_tensors='pt')
-    outputs = model.generate(inputs, max_length=128, num_return_sequences=1)
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
 
 
 test_questions = [
