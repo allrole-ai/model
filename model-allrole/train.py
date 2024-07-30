802a35ca84f1be4926ddb320177ea12b125fbb91
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 00:49:02 +0700

    delete training arguments for the Trainer

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -21,17 +21,7 @@
 
 
 
-# Langkah 3: Pelatihan Model
-training_args = TrainingArguments(
-    output_dir='./results',
-    num_train_epochs=3,
-    per_device_train_batch_size=4,
-    per_device_eval_batch_size=4,
-    warmup_steps=500,
-    weight_decay=0.01,
-    logging_dir='./logs',
-    logging_steps=10,
-)
+
 
 data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer,
