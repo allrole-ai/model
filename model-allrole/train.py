357f6a0da5db4a73df5e98b61655512c67f1e32d
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 00:55:11 +0700

    Split the data into train and test sets

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -8,3 +8,5 @@ from transformers import pipeline
 qa_data = pd.read_csv('dataset/qa-dataset.csv', delimiter='|')
 
 qa_data = qa_data.dropna().reset_index(drop=True)
+
+train_texts, val_texts, train_labels, val_labels = train_test_split(qa_data['question'], qa_data['answer'], test_size=0.2, random_state=42)
