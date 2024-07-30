ddad3edbb3dbe10081baf3b4273ac178d52d2ebe
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 00:38:50 +0700

    Define function to generate a response based on the input question using the model and tokenizer

--- model-allrole/testing.py
+++ model-allrole/testing.py
@@ -5,3 +5,6 @@ def load_model_and_tokenizer(model_path='./trained_model'):
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     model = AutoModelForCausalLM.from_pretrained(model_path)
     return tokenizer, model
+
+def generate_response(tokenizer, model, question, max_length=128):
+    inputs = tokenizer.encode(question, return_tensors='pt')
