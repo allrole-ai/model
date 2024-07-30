d516994d6fbf9bea7be25f977e14b13ccceb96cb
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 00:20:41 +0700

    delete function to answer new questions

--- model-allrole/training.py
+++ model-allrole/training.py
@@ -31,7 +31,3 @@
 # Fungsi untuk menjawab pertanyaan baru
 nlp = pipeline('text-classification', model=model, tokenizer=tokenizer, return_all_scores=True)
 
-def answer_question(question, context=[]):
-    """
-    Menjawab pertanyaan berdasarkan konteks percakapan sebelumnya.
-    
