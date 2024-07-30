debfcd9d08fe2fb0a5fd038d411cb53f00eabb67
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 00:38:14 +0700

    Define function to load the model and tokenizer from a specified path

--- model-allrole/testing.py
+++ model-allrole/testing.py
@@ -1,2 +1,7 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+
+def load_model_and_tokenizer(model_path='./trained_model'):
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForCausalLM.from_pretrained(model_path)
+    return tokenizer, model
