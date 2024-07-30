9cb764ac4f8133027aef0cdadc973064069a1673
ryaasishlah <ryaasishlah123@gmail.com>
2024-07-31 00:39:08 +0700

    Define function to generate a response based on the input question using the model and tokenizer

--- model-allrole/testing.py
+++ model-allrole/testing.py
@@ -8,3 +8,5 @@ def load_model_and_tokenizer(model_path='./trained_model'):
 
 def generate_response(tokenizer, model, question, max_length=128):
     inputs = tokenizer.encode(question, return_tensors='pt')
+    outputs = model.generate(inputs, max_length=max_length, num_return_sequences=1)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
