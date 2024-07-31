3da7b74c72b8832dd1270399eb37bfbf531f1ca8
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-08-01 01:53:43 +0700

    update generate qa and token

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -192,3 +192,8 @@ tokenizer.save_pretrained('model_bert/qa_tokenizer')
 nlp = pipeline('text-classification', model=model, tokenizer=tokenizer, return_all_scores=True)
 
 def answer_question(question, context=[]):
+    context.append(question)
+    input_text = " ".join(context)
+    result = nlp(input_text)
+    label = max(result[0], key=lambda x: x['score'])['label']
+    return id2label[int(label.split('_')[-1])]
