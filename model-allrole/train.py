e3c3087ce26dca1e9f27d784585fc6df42f59b48
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 18:07:56 +0700

    Define function to filter valid rows

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -6,3 +6,6 @@ from torch.utils.data import Dataset
 df = pd.read_csv('dataset/qa-dataset.csv', delimiter='|', on_bad_lines='skip')
 
 df.columns = ['question', 'answer']
+
+def filter_valid_rows(row):
+    return len(row) == 2
