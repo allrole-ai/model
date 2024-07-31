348262c8367b68fbf46cda695a80bb98f83bc3fc
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-08-01 01:34:06 +0700

    add  model roberta ke  model dan update func

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -171,3 +171,9 @@ training_args = TrainingArguments(
     eval_strategy="epoch"
 )
 
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=val_dataset
+)
