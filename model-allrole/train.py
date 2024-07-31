2f4ad2cecaafab91f8f49bff95b44d6e994e2996
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 18:16:38 +0700

    Define function to generate response to a question

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -85,3 +85,8 @@ model.save_pretrained('./model')
 
 tokenizer.save_pretrained('./model')
 
+def generate_response(question):
+    inputs = tokenizer.encode(question, return_tensors='pt')
+    outputs = model.generate(inputs, max_length=128, num_return_sequences=1)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
