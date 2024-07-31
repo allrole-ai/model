d3df3bd9acf9a0451c4f5e16346fd694634d5b67
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 20:58:08 +0700

    Define function to answer questions based on context

--- model-allrole/training.py
+++ model-allrole/training.py
@@ -88,6 +88,23 @@ tokenizer.save_pretrained('model/qa_tokenizer')
 # Fungsi untuk menjawab pertanyaan baru
 nlp = pipeline('text-classification', model=model, tokenizer=tokenizer, return_all_scores=True)
 
+def answer_question(question, context=[]):
+    """
+    Menjawab pertanyaan berdasarkan konteks percakapan sebelumnya.
+    
+    Parameters:
+    - question (str): Pertanyaan yang diajukan.
+    - context (list): Daftar konteks percakapan sebelumnya.
+    
+    Returns:
+    - str: Jawaban yang dihasilkan oleh model.
+    """
+    context.append(question)
+    input_text = " ".join(context)
+    result = nlp(input_text)
+    label = max(result[0], key=lambda x: x['score'])['label']
+    return id2label[int(label.split('_')[-1])]
+
 
 
 
