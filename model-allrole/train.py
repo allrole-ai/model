a402e156af8a6b6e65312755c0c48fa0870c5518
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 18:05:33 +0700

    delete pengujian dan evaluasi model

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -1,11 +1,4 @@
 
-# Langkah 4: Pengujian dan Evaluasi Model
-def generate_response(question):
-    inputs = tokenizer.encode(question, return_tensors='pt')
-    outputs = model.generate(inputs, max_length=128, num_return_sequences=1)
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
-
-
 test_questions = [
     "cara bayar spp",
     "cara buka Menu"
