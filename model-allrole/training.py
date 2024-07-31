b03d98179d8d4c32a98668c4a9e84453d2b9ea5e
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 20:52:34 +0700

    define custom dataset class

--- model-allrole/training.py
+++ model-allrole/training.py
@@ -37,3 +37,12 @@ class QADataset(torch.utils.data.Dataset):
     def __init__(self, encodings, labels):
         self.encodings = encodings
         self.labels = labels
+        
+    
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels.iloc[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
