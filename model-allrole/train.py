56b4ee316b2ccd50e0f6b588f4ee2d13454c2be1
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 18:06:44 +0700

    Load QA dataset using pandas

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -2,3 +2,5 @@ import pandas as pd
 from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments, DataCollatorForLanguageModeling
 import torch
 from torch.utils.data import Dataset
+
+df = pd.read_csv('dataset/qa-dataset.csv', delimiter='|', on_bad_lines='skip')
