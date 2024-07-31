d9d1e293e795d3398f11e2c2448355bdde503c5d
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-08-01 01:53:29 +0700

    update generate qa

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -188,3 +188,7 @@ if not os.path.exists('model_bert'):
 model.save_pretrained('model_bert/qa_model')
 tokenizer.save_pretrained('model_bert/qa_tokenizer')
 
+# Fungsi untuk menjawab pertanyaan baru
+nlp = pipeline('text-classification', model=model, tokenizer=tokenizer, return_all_scores=True)
+
+def answer_question(question, context=[]):
