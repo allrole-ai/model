a7deae101b5f1fdcb2108eba55ccc163afd82cab
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-08-01 01:44:24 +0700

    update new model bert

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -127,3 +127,6 @@ qa_data = pd.concat([qa_data, augmented_data], ignore_index=True)
 # Pra-pemrosesan data
 qa_data = qa_data.dropna().reset_index(drop=True)
 
+# Split data menjadi train dan test
+train_texts, val_texts, train_labels, val_labels = train_test_split(qa_data['question'], qa_data['answer'], test_size=0.2, random_state=42)
+
