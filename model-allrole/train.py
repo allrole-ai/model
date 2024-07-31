52085da4569b7ab4cbaf1447ed032a6b734a533f
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 18:03:17 +0700

    delete

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -1,27 +1,3 @@
-
-class CustomDataset(Dataset):
-    def __init__(self, questions, answers, tokenizer, max_length=128):
-        self.questions = questions
-        self.answers = answers
-        self.tokenizer = tokenizer
-        self.max_length = max_length
-
-    def __len__(self):
-        return len(self.questions)
-
-    def __getitem__(self, idx):
-        question = self.questions[idx]
-        answer = self.answers[idx]
-
-        inputs = self.tokenizer(question, return_tensors='pt', padding='max_length', max_length=self.max_length, truncation=True)
-        outputs = self.tokenizer(answer, return_tensors='pt', padding='max_length', max_length=self.max_length, truncation=True)
-
-        input_ids = inputs['input_ids'].squeeze(0)  # Remove batch dimension
-        attention_mask = inputs['attention_mask'].squeeze(0)
-        labels = outputs['input_ids'].squeeze(0)
-
-        return {'input_ids': input_ids, 'attention_mask': attention_mask, 'labels': labels}
-
 # Buat dataset
 dataset = CustomDataset(df['question'].tolist(), df['answer'].tolist(), tokenizer)
 
