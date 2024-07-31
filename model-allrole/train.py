0aaf4eb70dc00830dac903376d81765ebb501a21
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-08-01 01:41:25 +0700

    update model

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -146,3 +146,11 @@ class QADataset(torch.utils.data.Dataset):
         self.encodings = encodings
         self.labels = labels
         
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels.iloc[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
