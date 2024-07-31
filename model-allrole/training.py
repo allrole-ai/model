174182582c6f9bcf8ab9b115f07534e0690839d9
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 17:37:37 +0700

    delete

--- model-allrole/training.py
+++ model-allrole/training.py
@@ -76,23 +76,3 @@ if not os.path.exists('model'):
     os.makedirs('model')
 model.save_pretrained('model/qa_model')
 tokenizer.save_pretrained('model/qa_tokenizer')
-
-# Fungsi untuk menjawab pertanyaan baru
-nlp = pipeline('text-classification', model=model, tokenizer=tokenizer, return_all_scores=True)
-
-def answer_question(question, context=[]):
-    """
-    Menjawab pertanyaan berdasarkan konteks percakapan sebelumnya.
-    
-    Parameters:
-    - question (str): Pertanyaan yang diajukan.
-    - context (list): Daftar konteks percakapan sebelumnya.
-    
-    Returns:
-    - str: Jawaban yang dihasilkan oleh model.
-    """
-    context.append(question)
-    input_text = " ".join(context)
-    result = nlp(input_text)
-    label = max(result[0], key=lambda x: x['score'])['label']
-    return id2label[int(label.split('_')[-1])]
