382eac2dfbee144ba750f14f10ff2d4e2c488b8c
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 20:48:18 +0700

    Split data into train and test sets

--- model-allrole/training.py
+++ model-allrole/training.py
@@ -11,3 +11,6 @@ qa_data = pd.read_csv('dataset/qa-dataset.csv', delimiter='|')
 # Pra-pemrosesan data
 qa_data = qa_data.dropna().reset_index(drop=True)
 
+# Split data menjadi train dan test
+train_texts, val_texts, train_labels, val_labels = train_test_split(qa_data['question'], qa_data['answer'], test_size=0.2, random_state=42)
+
