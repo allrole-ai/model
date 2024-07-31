fd4d3407fc18006b731743d6472e9f4ee81da678
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 18:21:57 +0700

    delete vectorizer

--- my_qa_allrole/qa_model.py
+++ my_qa_allrole/qa_model.py
@@ -4,7 +4,4 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 
-# Memuat vectorizer dan data yang sudah disimpan
-with open('vectorizer.pkl', 'rb') as f:
-    vectorizer = pickle.load(f)
 
