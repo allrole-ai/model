a0848cb50412c0e92f77e03ab6db8ece9d36e964
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 20:44:06 +0700

    delete context

--- model-allrole/training.py
+++ model-allrole/training.py
@@ -19,11 +19,3 @@
 
 
 
-
-
-    """
-    context.append(question)
-    input_text = " ".join(context)
-    result = nlp(input_text)
-    label = max(result[0], key=lambda x: x['score'])['label']
-    return id2label[int(label.split('_')[-1])]
