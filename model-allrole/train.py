f16ae67d204d290f8c8d9085af591014eb7d5e80
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-08-01 01:45:09 +0700

    update generate qa and token

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -141,3 +141,7 @@ val_encodings = tokenizer(val_texts.tolist(), truncation=True, padding=True)
 # Konversi label menjadi angka
 label2id = {label: i for i, label in enumerate(qa_data['answer'].unique())}
 id2label = {i: label for label, i in label2id.items()}
+
+train_labels = train_labels.map(label2id)
+val_labels = val_labels.map(label2id)
+
