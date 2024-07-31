0cc2fad66bd2e30068ec3ddc45dd87cf67a9fc62
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-08-01 01:41:56 +0700

    update train for data model

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -157,3 +157,16 @@ class QADataset(torch.utils.data.Dataset):
 train_dataset = QADataset(train_encodings, train_labels)
 val_dataset = QADataset(val_encodings, val_labels)
 
+# Set up Trainer
+training_args = TrainingArguments(
+    output_dir='./results_roberta',
+    num_train_epochs=3,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    warmup_steps=500,
+    weight_decay=0.01,
+    logging_dir='./logs_roberta',
+    logging_steps=10,
+    eval_strategy="epoch"
+)
+
