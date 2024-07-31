db2a816a65181132497bd42f41d016291ded6109
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2024-08-01 01:43:59 +0700

    add new func

--- model-allrole/train.py
+++ model-allrole/train.py
@@ -110,3 +110,11 @@ for question in test_questions:
 
 
 
+import pandas as pd
+import torch
+import os
+from sklearn.model_selection import train_test_split
+from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification, Trainer, TrainingArguments, pipeline
+
+# Membaca dataset QA
+qa_data = pd.read_csv('dataset/qa-dataset.csv', delimiter='|')
