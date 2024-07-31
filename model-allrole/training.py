c3bee0f77679f2455185a6e77a0e68e7f6c0820e
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 20:43:08 +0700

    delete nlp

--- model-allrole/training.py
+++ model-allrole/training.py
@@ -18,8 +18,6 @@
 
 
 
-# Fungsi untuk menjawab pertanyaan baru
-nlp = pipeline('text-classification', model=model, tokenizer=tokenizer, return_all_scores=True)
 
 def answer_question(question, context=[]):
     """
