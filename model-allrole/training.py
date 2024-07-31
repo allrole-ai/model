cb6e46e0d3fdddf4b35605efbbeb2bea40689662
miqbalramadhan18 <miqbalr18@gmail.com>
2024-07-31 17:43:53 +0700

    delete read dataset qa

--- model-allrole/training.py
+++ model-allrole/training.py
@@ -5,8 +5,7 @@ from sklearn.model_selection import train_test_split
 from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification, Trainer, TrainingArguments
 from transformers import pipeline
 
-# Membaca dataset QA
-qa_data = pd.read_csv('dataset/qa-dataset.csv', delimiter='|')
+
 
 
 
