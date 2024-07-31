1b1168b68c8f9e5f7b6ced5af23333ea60e34c94
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 20:55:18 +0700

    set up trainer

--- model-allrole/training.py
+++ model-allrole/training.py
@@ -64,4 +64,11 @@ training_args = TrainingArguments(
     eval_strategy="epoch"  # Updated parameter name
 )
 
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=val_dataset
+)
+
 
