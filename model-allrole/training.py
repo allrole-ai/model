756eaf84593db240af5a8c2183c52a9488ef902d
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 20:43:31 +0700

    delete answer

--- model-allrole/training.py
+++ model-allrole/training.py
@@ -19,10 +19,7 @@
 
 
 
-def answer_question(question, context=[]):
-    """
-    Menjawab pertanyaan berdasarkan konteks percakapan sebelumnya.
-    
+
     Parameters:
     - question (str): Pertanyaan yang diajukan.
     - context (list): Daftar konteks percakapan sebelumnya.
