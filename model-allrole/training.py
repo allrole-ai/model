44e783bff3dcc9cd684483dca3c77df61da2e737
FarhanRiziq01 <farhanriziq01@gmail.com>
2024-07-31 20:41:17 +0700

    delete set up

--- model-allrole/training.py
+++ model-allrole/training.py
@@ -11,18 +11,7 @@
 
 
 
-# Set up Trainer
-training_args = TrainingArguments(
-    output_dir='./results',
-    num_train_epochs=3,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    warmup_steps=500,
-    weight_decay=0.01,
-    logging_dir='./logs',
-    logging_steps=10,
-    eval_strategy="epoch"  # Updated parameter name
-)
+
 
 trainer = Trainer(
     model=model,
